8d8074f1da13160db866f6c1ceb4a41b5b226c4a
Oluwatosin <tosinxware@gmail.com>
2023-03-16 12:42:55 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,49 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
  * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *p;
+	unsigned int i;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+	p = malloc(nmemb * size);
 
-	sout[i] = '\0';
+	if (p == NULL)
+		return (NULL);
 
-	return (sout);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
 }
